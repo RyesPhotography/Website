c2d268bb2961bd0a14af9f8138ddfcaf377661eb
RyesPhotography <ryes.photographyy@gmail.com>
2025-06-16 18:48:30 -0400

    Updated index.ts

--- src/data/index.ts
+++ src/data/index.ts
@@ -11,7 +11,7 @@ export const heroSlides: SlideImage[] = [ // Add image here under id: in src: ad
   },
   {
     id: "slide2",
-    src: "https://raw.githubusercontent.com/RyesPhotography/RyesPhotography-Images/refs/heads/master/sunflowerbridge.webp",
+    src: "https://raw.githubusercontent.com/RyesPhotography/RyesPhotography-Images/refs/heads/master/HD%20Sunflowers_result.webp",
     srcMobile: "https://raw.githubusercontent.com/RyesPhotography/RyesPhotography-Images/refs/heads/master/Mobile%20Sunflowers_result.webp",
     alt: "Portrait of a woman with dramatic lighting",
     title: "Portraits That Tell Your Story"
