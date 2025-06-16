d05c1050c7e4986d6a74ecf97532d7d98289e65e
RyesPhotography <ryes.photographyy@gmail.com>
2025-06-16 18:47:12 -0400

    Updated index.ts

--- src/data/index.ts
+++ src/data/index.ts
@@ -18,7 +18,7 @@ export const heroSlides: SlideImage[] = [ // Add image here under id: in src: ad
   },
   {
     id: "slide3",
-    src: "https://raw.githubusercontent.com/RyesPhotography/RyesPhotography-Images/refs/heads/master/birdintree.webp",
+    src: "https://raw.githubusercontent.com/RyesPhotography/RyesPhotography-Images/refs/heads/master/HD%20Bird_result.webp",
     srcMobile: "https://raw.githubusercontent.com/RyesPhotography/RyesPhotography-Images/refs/heads/master/mobile%20bird_result.webp",
     alt: "Breathtaking landscape of mountains at sunset",
     title: "Nature's Magnificent Beauty"
