c677570290cd7eb2b087fda703531c2f9b7161a8
RyesPhotography <ryes.photographyy@gmail.com>
2025-06-18 14:53:30 -0400

    Updated index.ts

--- src/data/index.ts
+++ src/data/index.ts
@@ -8,8 +8,7 @@ export const heroSlides: SlideImage[] = [ // Add image here under id: in src: ad
     srcMobile: "https://raw.githubusercontent.com/RyesPhotography/RyesPhotography-Images/refs/heads/master/Mobile%20flower_result.webp",
     alt: "Wedding couple kissing under a forest canopy",
     title: "Capturing Life's Beautiful Moments",
-    backgroundPosition: "center center", // Centers the flower properly
-    backgroundSize: "cover" // You can change this to "contain" if needed
+    backgroundPosition: "center 40%" // This focuses on the flower center, 40% down from top
   },
   {
     id: "slide2",
